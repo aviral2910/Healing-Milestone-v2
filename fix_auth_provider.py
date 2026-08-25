49caacc23b482626e315fb7955513448a0b84a95
Aviral <aviral@prmsnls.xyz>
2026-08-26 01:53:22 +0530

    chore: clean up a large number of leftover python script files from the repository

--- fix_auth_provider.py
+++
@@ -1,7 +0,0 @@
-with open("lib/features/auth/data/auth_provider.dart", "r") as f:
-    content = f.read()
-
-content = content.replace("currentState.copyWith(", "(state.value ?? const AuthState()).copyWith(")
-
-with open("lib/features/auth/data/auth_provider.dart", "w") as f:
-    f.write(content)
--- fix_verify.py
+++
@@ -1,7 +0,0 @@
-with open("lib/features/settings/presentation/screens/settings_screen.dart", "r") as f:
-    content = f.read()
-
-content = content.replace("await ref.read(authProvider.notifier).verifyPhoneNumber(phoneNumber);", "await ref.read(authProvider.notifier).verifyPhoneNumber(phoneNumber, onCodeSent: () {});")
-
-with open("lib/features/settings/presentation/screens/settings_screen.dart", "w") as f:
-    f.write(content)
